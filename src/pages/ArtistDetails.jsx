a050d6b53deb3b6d8c96b13cf6398a11ae25100a
Vahen1981 <fevahen@gmail.com>
2025-01-08 14:53:43 -0300

    mejoras

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,4 +1,4 @@
-import { CircularProgress, Typography } from '@mui/material';
+import { Button, CircularProgress, Grid2, Link, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
@@ -6,6 +6,7 @@ import { useParams } from 'react-router-dom';
 const ArtistDetails = () => {
     const { id } = useParams();
     const [info, setInfo] = useState(null);
+    const [tracks, setTracks] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
@@ -15,6 +16,11 @@ const ArtistDetails = () => {
                 const response = await fetch(`http://localhost:3000/api/artist/${id}`);
                 const data = await response.json();
                 setInfo(data);
+
+                const trackResponse = await fetch(`http://localhost:3000/api/artist/tracklist/${id}`);
+                const trackData = await trackResponse.json();
+                setTracks(trackData.data);
+
             } catch (error) {
                 setError("Error al cargar los artistas");
                 console.error("Error fetching artists:", error);
@@ -31,9 +37,51 @@ const ArtistDetails = () => {
             {error && <Typography color="error" sx={{ textAlign: 'center' }}>{error}</Typography>}
 
             {!loading && !error && (
-                <Typography variant="h4" sx={{ textAlign: 'center', marginBottom: 2 }}>
-                    {info.name}
-                </Typography>
+
+                <div style={{ padding: '20px' }}>
+                    <Typography variant="h4" sx={{ textAlign: 'center', marginBottom: 2 }}>
+                        {info.name}
+                    </Typography>
+
+                    <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+                        <img src={info.picture_big} alt={info.name} style={{ maxWidth: '100%', height: 'auto' }} />
+                    </div>
+
+                    <Grid2 container spacing={3} sx={{ justifyContent: 'center' }}>
+                        <Grid2 xs={12} md={4} sx={{ textAlign: 'center' }}>
+                            <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                                Número de fans
+                            </Typography>
+                            <Typography variant="body1">
+                                {info.nb_fan.toLocaleString()}
+                            </Typography>
+                        </Grid2>
+
+                        <Grid2 xs={12} md={4} sx={{ textAlign: 'center' }}>
+                            <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                                Enlace de Deezer
+                            </Typography>
+                            <Link href={info.link} target="_blank" rel="noopener">
+                                <Button variant="contained" color="primary">
+                                    Escuchar en Deezer
+                                </Button>
+                            </Link>
+                        </Grid2>
+
+                        <Grid2 xs={12} md={4} sx={{ textAlign: 'center' }}>
+                            <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                                Tracks más populares
+                            </Typography>
+                            <ul>
+                                {tracks.map((track) => (
+                                    <li key={track.id}>
+                                        <Typography variant="body1">{track.title}</Typography>
+                                    </li>
+                                ))}
+                            </ul>
+                        </Grid2>
+                    </Grid2>
+                </div>
             )}
         </div>
     );
