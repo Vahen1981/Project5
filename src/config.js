e0880c767fc602307a51dc8ef2ce2070cf871fbd
Vahen1981 <fevahen@gmail.com>
2025-01-11 18:49:30 -0300

    server online

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 //Url en servidor local
-export const API_URL = 'http://localhost:3000/api/';
+//export const API_URL = 'http://localhost:3000/api/';
 
 //Url en servidor en render
-//export const API_URL = 'https://servidor-para-deezer.onrender.com/api/';
+export const API_URL = 'https://servidor-para-deezer.onrender.com/api/';
